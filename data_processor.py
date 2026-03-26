1296841455274df0dd18a7e9bdeca02eb3645496
SannMyatMin <sanmyatmin88@gmail.com>
2026-03-26 11:56:34 +0630

    intro commit

--- data_processor.py
+++ data_processor.py
@@ -1,5 +1,13 @@
 import os
-import random
-import shutil
+import numpy as np
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.preprocessing import image
 
+class PaddyDiseaseDataProcessor:
+    def __init__(self, dataset_dir="paddy_disease_classification", img_size=224):
+        self.dataset_dir = dataset_dir
+        self.csv         = os.path.join(self.dataset_dir, "train.csv")
+        self.train_ds    = None
+        self.val_ds      = None
+        
