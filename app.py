b9bbfbfc8216fb4165c02b3a732c411533c7e87c
SannMyatMin <sanmyatmin88@gmail.com>
2026-03-30 11:09:16 +0630

    update path

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+root_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, root_path)
 
 import streamlit as st
 from streamlit_option_menu import option_menu
